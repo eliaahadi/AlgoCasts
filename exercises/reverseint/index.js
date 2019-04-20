f764c310423e7ebff476a13b58f56d52fe4742e2
eliagitai <elia.ahadi@gmail.com>
2019-04-20 13:49:06 +0900

    reverseInt solved

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,30 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let reverse = n.toString()
+    console.log("input ", reverse)
+
+    let outstring = '';\
+    for (let i = reverse.length - 1; i >= 0; i--) {
+
+        if (reverse[i] != '-') {
+            outstring += reverse[i]
+        }
+        console.log("loop ", i, outstring)
+    }
+    let outnum = parseInt(outstring)
+
+    if (reverse[0] === '-') {
+        console.log("test", reverse[0])
+        outnum = outnum * -1;
+    }
+
+
+    console.log("output string ", reverse[0], outstring, outnum)
+
+
+    return outnum;
+}
 
 module.exports = reverseInt;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,15 +7,15 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  //initialize empty string
-  let newStr = '';
-  //add the letters of str in reverse to newStr
-  for (i=str.length-1; i>=0; i--){
-    console.log(i,str[i]);
-    newStr = newStr+str[i];
-  }
-  console.log("newStr SHOWS ", newStr);
-  return newStr;
+    //initialize empty string
+    let newStr = '';
+    //add the letters of str in reverse to newStr
+    for (let i = str.length - 1; i >= 0; i--) {
+        console.log(i, str[i]);
+        newStr = newStr + str[i];
+    }
+    console.log("newStr SHOWS ", newStr);
+    return newStr;
 }
 
 module.exports = reverse;
